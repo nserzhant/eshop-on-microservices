2322cf7fbc6ad6156d8c16e02533585c5abf9e60
nserzhant <nick.serzhant@gmail.com>
2025-05-08 23:11:45 +0300

    Add default credentials to the login screens

--- eshop-api/Customer/src/EShop.Customer.AuthorizationServer/Controllers/AccountController.cs
+++ eshop-api/Customer/src/EShop.Customer.AuthorizationServer/Controllers/AccountController.cs
@@ -95,7 +95,7 @@ public class AccountController : Controller
         {
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+            var result = await _signInManager.PasswordSignInAsync(model.Email.Trim(), model.Password.Trim(), model.RememberMe, lockoutOnFailure: false);
             if (result.Succeeded)
             {
                 _logger.LogInformation("User logged in.");
